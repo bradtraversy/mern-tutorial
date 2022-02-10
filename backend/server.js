76f97fec6e14eb39855e0486b85f224a53c30d4d
Brad Traversy <traversymedia@gmail.com>
2022-02-10 13:06:06 -0500

    Prepare server.js for deployment

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,19 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  )
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'))
+}
+
 app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))

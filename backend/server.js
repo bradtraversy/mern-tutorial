accb473817f977c27a474a301d02c5bf013d5eb5
Christina Voudouris <christina.voudouris@gmail.com>
2022-02-12 23:00:35 -0500

    add const for path

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')

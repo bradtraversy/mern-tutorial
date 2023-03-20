d122ff12d941d738f400ff2535ad407de71d33e8
m-housni <mohamed.housni@um5s.net.ma>
2023-03-20 13:22:55 +0000

    add server side email validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,9 +9,17 @@ const User = require('../models/userModel')
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body
 
+  // Check for empty fields
   if (!name || !email || !password) {
     res.status(400)
     throw new Error('Please add all fields')
+  }
+
+  // email validation
+  let emailRegExp = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+  if (!regex.test(emailRegExp)) {
+    res.status(400)
+    throw new Error("Email is not valid")
   }
 
   // Check if user exists

d63c1542204aff7494c78e2d75269f609e19dea6
Mrinalini Nair Ani <91671136+hacksh4w@users.noreply.github.com>
2023-06-09 05:42:55 +0530

    Update goalController.js
    .remove() fn for delete goal does not work, According to the MongoDB Forum, it is deleteOne() instead.

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -82,7 +82,7 @@ const deleteGoal = asyncHandler(async (req, res) => {
     throw new Error('User not authorized')
   }
 
-  await goal.remove()
+  await goal.deleteOne()
 
   res.status(200).json({ id: req.params.id })
 })
